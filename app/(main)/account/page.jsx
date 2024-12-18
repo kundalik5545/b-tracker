4107fec1d2cfa4279740badb733dd869df75c9af
kundalik jadhav <kundalikjadhav0807@gmail.com>
2024-12-18 09:24:39 -0800

    Working on dashboard add account

---
+++ app/(main)/account/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>Account Page</div>;
+}
+
+export default Page;
---
+++ app/(main)/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authlayout = ({ children }) => {
+  return <div className="container mx-auto my-32">{children}</div>;
+};
+
+export default Authlayout;
